bd6cd26a4a033d0cdee2ad6fd577476476d6d0d6
Vojtech Sebek <vojtech.sebek@plus4u.net>
2017-12-09 18:56:10 +0100

    lesson form - teacher select

--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -8,4 +8,10 @@ class Lesson < ApplicationRecord
     ROOMS_NAMES["#{b.title}"] = b.id
   end
 
+  TEACHERS_NAMES = {}
+  teachers = Teacher.all
+  teachers.each do |b|
+    TEACHERS_NAMES["#{b.first_name} #{b.last_name}"] = b.id
+  end
+
 end
