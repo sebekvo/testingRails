09d7cba7a4fa5393b660e743b2475a30a0904ca6
Vojtech Sebek <vojtech.sebek@plus4u.net>
2017-12-09 20:08:41 +0100

    updated teacher-course form

--- app/models/teacher_course.rb
+++ app/models/teacher_course.rb
@@ -1,4 +1,17 @@
 class TeacherCourse < ApplicationRecord
   belongs_to :teacher
   belongs_to :course
+
+  COURSES_NAMES = {}
+  courses = Course.all
+  courses.each do |b|
+    COURSES_NAMES["#{b.title}"] = b.id
+  end
+
+  TEACHERS_NAMES = {}
+  teachers = Teacher.all
+  teachers.each do |b|
+    TEACHERS_NAMES["#{b.first_name} #{b.last_name}"] = b.id
+  end
+
 end
