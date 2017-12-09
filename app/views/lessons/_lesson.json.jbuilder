36b218ba01d220400f1de8ebf1dbde25f6e9bcda
Vojtech Sebek <vojtech.sebek@plus4u.net>
2017-12-09 18:30:38 +0100

    added lesson

---
+++ app/views/lessons/_lesson.json.jbuilder
@@ -0,0 +1,2 @@
+json.extract! lesson, :id, :start_at, :end_at, :duration, :teacher_id, :room_id, :created_at, :updated_at
+json.url lesson_url(lesson, format: :json)
---
+++ db/migrate/20171209173020_create_lessons.rb
@@ -0,0 +1,13 @@
+class CreateLessons < ActiveRecord::Migration[5.1]
+  def change
+    create_table :lessons do |t|
+      t.datetime :start_at
+      t.datetime :end_at
+      t.integer :duration
+      t.integer :teacher_id
+      t.integer :room_id
+
+      t.timestamps
+    end
+  end
+end
