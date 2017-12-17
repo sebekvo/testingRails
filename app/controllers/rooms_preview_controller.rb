6a8794e98164e0355653daa57a0b13f1243296d9
Vojtech Sebek <vojtech.sebek@plus4u.net>
2017-12-17 04:43:29 +0100

    added check if no content

--- app/controllers/rooms_preview_controller.rb
+++ app/controllers/rooms_preview_controller.rb
@@ -1,6 +1,7 @@
 class RoomsPreviewController < ApplicationController
 
   def show
+    @thisRoom = Room.find(params[:id])
     @lessons = Lesson.where(:room_id => params[:id])
     @lessonsCount = @lessons.count
   end
